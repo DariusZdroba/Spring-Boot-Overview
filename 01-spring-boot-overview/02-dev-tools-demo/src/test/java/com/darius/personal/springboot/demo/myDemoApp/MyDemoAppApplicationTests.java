7326a8a56f5374426399f2f79ed482edbbf00363
Zdroba <72391375+DariusZdroba@users.noreply.github.com>
2024-03-05 14:03:35 +0200

    Add files via upload

---
+++ 01-spring-boot-overview/02-dev-tools-demo/src/test/java/com/darius/personal/springboot/demo/myDemoApp/MyDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darius.personal.springboot.demo.myDemoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
